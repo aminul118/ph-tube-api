d2640ad0e6c0af949094a17fd0f3ffc010288ba5
Md Aminul Islam <mr.aminul118@gmail.com>
2024-10-05 17:14:47 +0600

    formatting change

--- scripts/video.js
+++ scripts/video.js
@@ -117,37 +117,33 @@ const displayVideos = (videos) => {
         src=${video.thumbnail}
         class="h-full w-full object-cover"
         alt="Shoes" />
-        ${
-          video.others.posted_date?.length == 0
-            ? ""
-            : `<span class="absolute text-xs right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(
-                video.others.posted_date
-              )}</span>`
-        }
+        ${video.others.posted_date?.length == 0
+        ? ""
+        : `<span class="absolute text-xs right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(
+          video.others.posted_date
+        )}</span>`
+      }
         
     </figure>
     <div class="px-0 py-2 flex gap-2">
         <div>
-            <img class="w-10 h-10 rounded-full object-cover" src=${
-              video.authors[0].profile_picture
-            } />
+            <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture
+      } />
         </div>
         <div>
         <h2 class="font-bold">${video.title}</h2>
         <div class="flex items-center gap-2">
             <p class="text-gray-400">${video.authors[0].profile_name}</p>
 
-            ${
-              video.authors[0].verified == true
-                ? `<img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>`
-                : ""
-            }
+            ${video.authors[0].verified == true
+        ? `<img class="w-5" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>`
+        : ""
+      }
 
             
         </div>
-        <p> <button  onclick="loadDetails('${
-          video.video_id
-        }')" class="btn btn-sm btn-error">details</button> </p>
+        <p> <button  onclick="loadDetails('${video.video_id
+      }')" class="btn btn-sm btn-error">details</button> </p>
         </div>
     </div>
     `;
